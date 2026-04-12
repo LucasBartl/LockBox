f30ff3d6ffcc417947a7fa63cb3cd7547063ecab
Lucas Rodrigues <lucas.bartllb@gmail.com>
2026-04-12 12:34:01 -0300

    refatorando

---
+++ App/Controllers/indexController.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace App\controllers;
+
+
+class IndexController
+{
+    public function __invoke()
+    {
+        echo "indexcontroller.__invoke";
+    }
+}
---
+++ App/Controllers/loginController.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace App\controllers;
+
+
+class LoginController
+{
+    public function index(){
+        echo "login";
+    }
+    public function login(){
+        echo "login login";
+    }
+}
